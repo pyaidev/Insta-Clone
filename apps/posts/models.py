1660ab940d7a04c9acda76e46ca737c587234daa
sobirjonovme <huntsman6004@gmail.com>
2023-03-30 15:56:13 +0500

    fix: git comflict

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -13,9 +13,26 @@ class Post(TimeStampedModel):
         verbose_name=_("user"),
     )
     description = RichTextField()
+    tags = models.ArrayField(models.CharField(max_length=255))
+    is_comment = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.user.username} | {self.description[:16]}..."
+
+    @property
+    def likes_count(self):
+        like = Like.objects.filter(post_id=self.id).count()
+        return like
+
+    @property
+    def comments_count(self):
+        comment = Comment.objects.filter(post_id=self.id).count()
+        return comment
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
 
 
 class PostTag(TimeStampedModel):
@@ -24,3 +41,57 @@ class PostTag(TimeStampedModel):
 
     def __str__(self):
         return f"{self.tag.name} | {self.post}"
+
+
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.db.models import FileField, ForeignKey, CASCADE, TextField, CharField, BooleanField, SET_NULL, IntegerField, \
+    Count, PROTECT
+
+
+class PostMedia(TimeStampedModel):
+    file = FileField(upload_to='posts/')
+
+    class Meta:
+        verbose_name = 'PostMedia'
+        verbose_name_plural = 'PostMedia'
+
+
+class Post(BaseModel):
+    file = ForeignKey('posts.PostMedia', PROTECT)
+    author = ForeignKey('profiles.Profile', CASCADE)
+    description = TextField()
+    tags = ArrayField(CharField(max_length=255))
+    is_comment = BooleanField(default=True)
+
+    def __str__(self):
+        return self.description
+
+
+
+
+class Comment(BaseModel):
+    user = ForeignKey('profiles.Profile', CASCADE)
+    post = ForeignKey('posts.Post', CASCADE, limit_choices_to={'is_comment': True})
+    parent = ForeignKey('self', SET_NULL, null=True, blank=True, related_name='replies')
+    text = TextField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Comments'
+        verbose_name_plural = 'Comments'
+
+
+class Like(BaseModel):
+    user = ForeignKey('profiles.Profile', CASCADE)
+    post = ForeignKey('posts.Post', CASCADE)
+
+    def __str__(self):
+        return self.post.description
+
+    class Meta:
+        verbose_name = 'Likes'
+        verbose_name_plural = 'Likes'
