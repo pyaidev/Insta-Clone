8737d9fcddf011dbafe90bdfc43d453d784f498b
muhammadjabborov <zabboorovmuhammad291@gmail.com>
2023-03-31 16:49:52 +0500

    fix: base.html changed

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.profiles.models import Profile, Follower
+
+admin.site.register(Profile)
+admin.site.register(Follower)
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
 
-# Create your views here.
+from apps.profiles.models import Profile
+
+
+class ProfileDetailTemplateView(LoginRequiredMixin, DetailView):
+    template_name = 'main/profile.html'
+    slug_field = 'username'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = Profile.objects.get(user=self.request.user)
+        context['profile'] = profile
+        return context
