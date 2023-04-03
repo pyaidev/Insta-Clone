118d57eec24cf6f263ff5190521914b239bec6fc
sobirjonovme <huntsman6004@gmail.com>
2023-04-03 13:29:32 +0500

    feat: models registered to admin panel

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import PostMedia, Post, PostLike
+from .models import PostMedia, Post, PostLike, Comment, PostTag
 
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(PostMedia)
 admin.site.register(PostLike)
+admin.site.register(Comment)
+admin.site.register(PostTag)
