88f7a2a8dc78e6cf009118d65980cd9ce110b77d
muhammadjabborov <zabboorovmuhammad291@gmail.com>
2023-04-08 14:44:19 +0500

    fix : add story template

--- apps/stories/models.py
+++ apps/stories/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import TimeStampedModel
+from apps.posts.choices import MediaTypes
+from apps.profiles.models import Profile
+
+
+class Story(TimeStampedModel):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.user.username
+
+    class Meta:
+        verbose_name = "Story"
+        verbose_name_plural = "Story"
+
+
+class StoryContent(TimeStampedModel):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="story/%Y/%m/%d/")
+    type = models.CharField(max_length=6, choices=MediaTypes.choices, default=MediaTypes.IMAGE)
+
+    def __str__(self):
+        return str(self.story.id)
+
+    class Meta:
+        verbose_name = "ContentStory"
+        verbose_name_plural = "ContentStory"
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, register, profile, verify_email
+from .views import login_view, profile
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('signup/', register, name='register'),
-    path('profile/', profile, name='profile_create'),
-    path('verificate/', verify_email, name='verify_email')
+    # path('signup/', register, name='register'),
+    path('profile/', profile, name='profile_create')
+    # path('verificate/', verify_email, name='verify_email')
 ]
