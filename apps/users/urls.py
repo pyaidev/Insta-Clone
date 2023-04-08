8109f5e37a0ba3d58a2691b0f55a74b4afec1818
sobirjonovme <huntsman6004@gmail.com>
2023-04-08 15:23:22 +0500

    fix: user urls changed

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', RegisterView.as_view(), name='register'),
     path('profile/', profile, name='profile_create'),
-    path('verify/', verify_email, name='verify_email')
+    path('verify/', verify_email, name='verify_email'),
+
+    ]
