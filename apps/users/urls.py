66fa79101bcd92dcad914d7f12dd99960e1845e8
sobirjonovme <huntsman6004@gmail.com>
2023-04-08 15:20:53 +0500

    feat: login verify added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, register, profile, verify_email
+from .views import login_view, RegisterView, profile, verify_email
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('signup/', register, name='register'),
+    path('signup/', RegisterView.as_view(), name='register'),
     path('profile/', profile, name='profile_create'),
-    path('verificate/', verify_email, name='verify_email')
+    path('verify/', verify_email, name='verify_email')
 ]
